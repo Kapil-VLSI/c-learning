03732f4662430275fb97ea6cbc8bb81cbce72b13
Kapil-VLSI <kapil.uranium.235@gmail.com>
2026-05-26 14:26:10 +0530

    Update 03-student-pass-fail.c

--- 02-decision-Making/03-student-pass-fail.c
+++ 02-decision-Making/03-student-pass-fail.c
@@ -8,7 +8,7 @@ int main(int argc, char const *argv[])
     scanf("%d",&physics);
     printf("enter the marks of chemistry\n");
 scanf("%d",&chemistry);
-float total_percectage= (maths+physics+chemistry)/300.0*100;
+float total_percentage= (maths+physics+chemistry)/300.0*100;
 if(maths>=33 && physics>=33 && chemistry>=33 && total_percectage>=40){
     printf("student is pass");
 }
